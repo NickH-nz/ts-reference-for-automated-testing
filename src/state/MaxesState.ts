4a40b97db4d414b72ae7e466f4cb63bec9786399
Nick Heath <nicholas.heath.nz@gmail.com>
2017-07-01 15:07:38 +1200

    Make UI mobile friendly for both portrait and landscape.

--- src/state/MaxesState.ts
+++ src/state/MaxesState.ts
@@ -19,6 +19,10 @@ export class MaxesState extends Phaser.State {
     private expBar: ExperienceBar;
     private endGameMessage: Phaser.Text;
 
+    private boundaryInput: Phaser.Rectangle;
+    private boundaryHistory: Phaser.Rectangle;
+    private boundaryExp: Phaser.Rectangle;
+
     public preload(): void {
         this.load.spritesheet("rps", "assets/rps.png", 289, 275);
         this.load.image("rps-select", "assets/rps-combined.png");
@@ -33,6 +37,17 @@ export class MaxesState extends Phaser.State {
     }
 
     public create(): void {
+        this.setupBoundaries();
+
+        const padding: number = this.game.width * 0.01;
+        this.expBar = new ExperienceBar(this.game, padding, padding, new ExperienceTracker());
+        this.expBar.height = this.boundaryExp.height;
+        this.expBar.width = this.boundaryExp.width;
+        this.expBar.scale.set(Math.min(this.expBar.scale.x, this.expBar.scale.y));
+        this.expBar.x = this.boundaryExp.x;
+        this.expBar.centerY = this.boundaryExp.centerY;
+        this.add.existing(this.expBar);
+
         this.p1Moves = this.add.group();
         this.p2Moves = this.add.group();
 
@@ -41,11 +56,11 @@ export class MaxesState extends Phaser.State {
             new InputOption(RpsMove.PAPER, "rps", 1),
             new InputOption(RpsMove.SCISSORS, "rps", 2),
         ]);
-        rps.width = this.game.width * 0.5;
-        rps.height = this.game.height * 0.5;
+        rps.width = this.boundaryInput.width * 0.6;
+        rps.height = this.boundaryInput.height * 0.8;
         rps.scale.set(Math.min(rps.scale.x, rps.scale.y));
-        rps.centerX = this.game.width * 0.5;
-        rps.centerY = this.game.height * 0.6;
+        rps.centerX = this.boundaryInput.centerX;
+        rps.centerY = this.boundaryInput.centerY;
         rps.visible = false;
         rps.onMoveSelected.add(async (move: RpsMove) => {
             const p2Move: RpsMove = this.game.rnd.pick([RpsMove.ROCK, RpsMove.PAPER, RpsMove.SCISSORS]);
@@ -65,11 +80,11 @@ export class MaxesState extends Phaser.State {
             new InputOption(DirectionMove.LEFT, "directions", 2),
             new InputOption(DirectionMove.DOWN, "directions", 3),
         ]);
-        directions.width = this.game.width * 0.5;
-        directions.height = this.game.height * 0.5;
+        directions.width = this.boundaryInput.width * 0.6;
+        directions.height = this.boundaryInput.height * 0.8;
         directions.scale.set(Math.min(directions.scale.x, directions.scale.y));
-        directions.centerX = this.game.width * 0.5;
-        directions.centerY = this.game.height * 0.6;
+        directions.centerX = this.boundaryInput.centerX;
+        directions.centerY = this.boundaryInput.centerY;
         directions.visible = false;
         directions.onMoveSelected.add(async (move: DirectionMove) => {
             const p2Move: DirectionMove = this.game.rnd.pick([
@@ -104,11 +119,11 @@ export class MaxesState extends Phaser.State {
         this.add.existing(rps);
 
         const rpsSelector: Phaser.Image = new Phaser.Image(this.game, 0, 0, "rps-select");
-        rpsSelector.width = this.game.width * 0.25;
-        rpsSelector.height = this.game.height * 0.8 * 0.5;
+        rpsSelector.width = this.boundaryInput.halfWidth * 0.6;
+        rpsSelector.height = this.boundaryInput.height * 0.6;
         rpsSelector.scale.set(Math.min(rpsSelector.scale.x, rpsSelector.scale.y));
-        rpsSelector.centerX = this.game.width * 0.3;
-        rpsSelector.centerY = this.game.height * 0.6;
+        rpsSelector.centerX = this.boundaryInput.x + (this.boundaryInput.width * 0.3);
+        rpsSelector.centerY = this.boundaryInput.centerY;
         rpsSelector.inputEnabled = true;
         rpsSelector.events.onInputUp.add(() => {
             if (this.isGameInProgress()) {
@@ -120,11 +135,11 @@ export class MaxesState extends Phaser.State {
         this.add.existing(rpsSelector);
 
         const directionSelector: Phaser.Image = new Phaser.Image(this.game, 0, 0, "direction-select");
-        directionSelector.width = this.game.width * 0.25;
-        directionSelector.height = this.game.height * 0.8 * 0.5;
+        directionSelector.width = this.boundaryInput.halfWidth * 0.6;
+        directionSelector.height = this.boundaryInput.height * 0.6;
         directionSelector.scale.set(Math.min(directionSelector.scale.x, directionSelector.scale.y));
-        directionSelector.centerX = this.game.width * 0.7;
-        directionSelector.centerY = this.game.height * 0.6;
+        directionSelector.centerX = this.boundaryInput.x + (this.boundaryInput.width * 0.7);
+        directionSelector.centerY = this.boundaryInput.centerY;
         directionSelector.inputEnabled = true;
         directionSelector.events.onInputUp.add(() => {
             if (this.isGameInProgress()) {
@@ -135,20 +150,40 @@ export class MaxesState extends Phaser.State {
         });
         this.add.existing(directionSelector);
 
-        const padding: number = this.game.width * 0.01;
-        this.expBar = new ExperienceBar(this.game, padding, padding, new ExperienceTracker());
-        this.expBar.width = this.game.width * 0.5;
-        this.expBar.height = this.game.height * 0.1;
-        this.expBar.scale.set(Math.min(this.expBar.scale.x, this.expBar.scale.y));
-        this.add.existing(this.expBar);
-
         this.endGameMessage = new Phaser.Text(this.game, 0, 0, "", {
             fill: "white",
-            fontSize: 20,
+            fontSize: this.boundaryInput.height * 0.2,
         });
+        this.endGameMessage.y = this.boundaryInput.top;
+        this.endGameMessage.centerX = this.boundaryInput.centerX;
         this.add.existing(this.endGameMessage);
 
         this.setupNewGame();
+    }
+
+    private setupBoundaries(): void {
+        const padding: number = Math.min(this.game.width * 0.01, this.game.height * 0.01);
+
+        this.boundaryExp = new Phaser.Rectangle(
+            padding,
+            padding,
+            this.game.width - (padding * 2),
+            (this.game.height * 0.2) - (padding * 2),
+        );
+
+        this.boundaryHistory = new Phaser.Rectangle(
+            padding,
+            (this.game.height * 0.2) + padding,
+            this.game.width - (padding * 2),
+            (this.game.height * 0.2) - (padding * 2),
+        );
+
+        this.boundaryInput = new Phaser.Rectangle(
+            padding,
+            this.game.height * 0.4 + padding,
+            this.game.width - (padding * 2),
+            (this.game.height * 0.6) - (padding * 2),
+        );
     }
 
     private addMoveToUi(p1Move: RpsMove | DirectionMove, p2Move: RpsMove | DirectionMove): void {
@@ -164,12 +199,13 @@ export class MaxesState extends Phaser.State {
                     break;
             }
             const newMove: Phaser.Image = new Phaser.Image(this.game, 0, 0, asset, p1Move.value);
-            newMove.width = this.game.width * 0.05;
-            newMove.scale.y = newMove.scale.x;
+            newMove.height = this.boundaryHistory.height * 0.6;
+            newMove.scale.x = newMove.scale.y;
             newMove.left = this.p1Moves.width;
             newMove.events.onOutOfBounds.add(newMove.destroy, newMove);
             this.p1Moves.add(newMove);
-            this.p1Moves.right = this.game.width * 0.45;
+            this.p1Moves.right = this.boundaryHistory.halfWidth * 0.95;
+            this.p1Moves.centerY = this.boundaryHistory.centerY;
         }
 
         if (p2Move !== null) {
@@ -184,14 +220,15 @@ export class MaxesState extends Phaser.State {
                     break;
             }
             const newMove: Phaser.Image = new Phaser.Image(this.game, 0, 0, asset, p2Move.value);
-            newMove.width = this.game.width * 0.05;
-            newMove.scale.y = newMove.scale.x;
+            newMove.height = this.boundaryHistory.height * 0.6;
+            newMove.scale.x = newMove.scale.y;
             newMove.events.onOutOfBounds.add(newMove.destroy, newMove);
             this.p2Moves.children.forEach((child) => {
                 child.x += newMove.width;
             });
             this.p2Moves.add(newMove);
             this.p2Moves.left = this.game.width * 0.55;
+            this.p2Moves.centerY = this.boundaryHistory.centerY;
         }
     }
 
